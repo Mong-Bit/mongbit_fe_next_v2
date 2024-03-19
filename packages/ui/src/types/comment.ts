66b8e812b89c2b0d5578b3f5ba8eab1b2d5784a2
Kim SongHyeon <thiagoo@naver.com>
2024-03-19 22:14:48 +0900

    refactor: 프리티어 적용

--- packages/ui/src/types/comment.ts
+++ packages/ui/src/types/comment.ts
@@ -1,23 +1,22 @@
+export type CommentDTO = {
+  id: string;
+  memberId: string;
+  testId: string;
+  commentDate: string;
+  content: string;
+  username: string;
+  thumbnailImage: string;
+};
 
-  export type CommentDTO = {
-    id: string;
-    memberId: string;
-    testId: string;
-    commentDate: string;
-    content: string;
-    username: string;
-    thumbnailImage: string;
-  }
+export type CommentData = {
+  id: string;
+  memberId: string;
+  testId: string;
+  commentDate: string;
+  content: string;
+};
 
-  export type CommentData = {
-    id: string;
-    memberId: string;
-    testId: string;
-    commentDate: string;
-    content: string;
-  }
-
-  export type CommentResponse = {
-    commentDTOList: [CommentDTO];
-    hasNextPage: boolean;
-  }
+export type CommentResponse = {
+  commentDTOList: [CommentDTO];
+  hasNextPage: boolean;
+};
