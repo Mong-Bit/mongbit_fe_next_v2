79ca616b92550bffc9972518235a1af310de6468
Crystal P <sj8871@gmail.com>
2024-04-05 22:56:30 +0900

    refactor : Contents Component Table columns
    1.  ColumnsType의 제네릭 타입 지정하기
     - 제네릭 타입으로 ContentList 사용
     - dataIndex 삭제 : 명시한 타입의 키값을 직접 사용하기 때문에 dataIndex를 사용하지 않아도 됨
     - render 두 번째 인자로 사용 : (property) render: (_: any, { imageUrl }: ContentList) => JSX.Element
      1) 첫번 째 인자는 항상 any가 옴
      2) 두번째 인자에서 정의 한 타입별로 데이터를 사용 가능
      ** any가 오지 않도록 항상 타입을 확인하며 사용 할 것 ( 타입 스크립트를 사용한다면 any는 최대한 피합시다! )
    
    2. columns로 가지고 있기
     - useMemo을 활용하여 반복되어 호출되는 함수 기억하기
    
    3. hook 컨벤션 안맞음
     - useResetMbtiTestData : 훅이 아닌데 use를 사용하여 컨벤션이 어긋남
     - resetMbtiTestData로 함수명 변경
    
    4. contentsTableColumns -> Contents Component / index.tsx 내부로 이동
     - 파일을 나눌 필요 없이 상단에서 호출하여 사용하도록 변경

--- apps/admin/containers/ContentsComponent/index.tsx
+++ apps/admin/containers/ContentsComponent/index.tsx
@@ -1,44 +1,131 @@
 'use client';
 
-import { Button, Select, Table } from 'antd';
+import { Button, Popconfirm, Select, Space, Table } from 'antd';
 import Search from 'antd/es/input/Search';
+import { ColumnsType } from 'antd/es/table';
 import cx from 'classnames';
+import Image from 'next/image';
 import { useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useSetRecoilState } from 'recoil';
 
-import { Paths } from '@/constants/paths';
+import { CONTENTS_COUNT_OPTIONS } from '@/constants/constant';
+import { Paths, PathsId } from '@/constants/paths';
 import { useContents } from '@/hooks/useContents';
 import { useImageUpload } from '@/hooks/useImageUpload';
 import { initialMbtiTestData, mbtiTestDataState, isEditContentState } from '@/states/contentUpdateState';
+import { ContentList } from '@/types/contents';
 
-import contentsTableColumns from './contentsTableColumns';
 import styles from './index.module.scss';
+
+const getColumns = ({
+  onClickDeleteBtn,
+  onClickEditBtn,
+}: {
+  onClickDeleteBtn: (testId: string) => void;
+  onClickEditBtn: (testId: string) => void;
+}): ColumnsType<ContentList> =>
+  [
+    {
+      title: 'Thumbnail',
+      key: 'imageUrl',
+      width: 120,
+      align: 'center',
+      render: (_, { imageUrl }) => (
+        <div style={{ width: '90px', height: '60px', overflow: 'hidden', position: 'relative', margin: 'auto' }}>
+          <Image src={imageUrl} fill sizes="100%" alt="testImage" priority quality={5} />
+        </div>
+      ),
+    },
+    {
+      title: 'Title',
+      key: 'title',
+      width: 250,
+      render: (_, { title }) => <a className={styles.contentTitle}>{title}</a>,
+    },
+    {
+      title: 'Counts',
+      children: CONTENTS_COUNT_OPTIONS.map((option) => ({
+        title: option.lable,
+        key: option.value,
+        width: 85,
+        align: 'center',
+      })),
+    },
+    {
+      title: 'Created Date',
+      key: 'createDate',
+      align: 'center',
+      width: 130,
+      render: (_, { createDate }) => <p>{new Date(createDate).toLocaleDateString('en-CA')}</p>,
+    },
+    {
+      title: '',
+      dataIndex: 'id',
+      key: 'id',
+      width: 120,
+      align: 'center',
+      fixed: 'right',
+      render: (_, { id }) => (
+        <Space>
+          <Button size="small" onClick={() => onClickEditBtn(id)}>
+            수정
+          </Button>
+          <Popconfirm
+            title="Delete the task"
+            description="Are you sure to delete this task?"
+            okText="Yes"
+            cancelText="No"
+            onConfirm={() => onClickDeleteBtn(id)}
+          >
+            <Button size="small" danger>
+              삭제
+            </Button>
+          </Popconfirm>
+        </Space>
+      ),
+    },
+  ];
 
 const pageSize = 5;
 
 export default function ContentsComponent() {
-  const { getContents, contentsData } = useContents();
+  const { getContents, deleteContent, contentsData } = useContents();
   const { deleteImageFileArray } = useImageUpload();
-  const resetMbtiTestData = useSetRecoilState(mbtiTestDataState);
+  const initializationMbtiTestData = useSetRecoilState(mbtiTestDataState);
   const setIsEditContent = useSetRecoilState(isEditContentState);
   const [page, setPage] = useState(0);
 
   const router = useRouter();
 
-  const useResetMbtiTestData = () => {
-    resetMbtiTestData(initialMbtiTestData);
+  const resetMbtiTestData = () => {
+    initializationMbtiTestData(initialMbtiTestData);
     deleteImageFileArray();
   };
 
-  const mbtiColumns = contentsTableColumns(useResetMbtiTestData);
-
   const onClickRegisterButton = () => {
-    useResetMbtiTestData();
+    resetMbtiTestData();
     setIsEditContent(false);
     router.push(Paths.contentsRegister);
   };
 
+  const columns = useMemo(
+    () =>
+      getColumns({
+        onClickDeleteBtn: (testId: string) => {
+          deleteContent(testId);
+          alert(`삭제 완료`);
+        },
+        onClickEditBtn: (testId: string) => {
+          resetMbtiTestData();
+          setIsEditContent(true);
+          router.push(PathsId(testId));
+        },
+      }),
+    [],
+  );
+
+  // TODO: SSR 적용할 수 있도록 확인하기
   useEffect(() => {
     getContents(page, pageSize);
   }, [page]);
@@ -49,12 +136,16 @@ export default function ContentsComponent() {
         <div className={styles.searchBox}>
           <Select style={{ width: 120 }} />
           <p>Member ID</p>
-          <Search placeholder="input search text" onSearch={(value) => console.log(value)} style={{ width: 300 }} />
+          <Search
+            placeholder="input search text"
+            onSearch={(value) => alert(`현재 지원하지 않아 ${value}을(를) 검색할 수 없습니다.`)}
+            style={{ width: 300 }}
+          />
         </div>
         <Button onClick={onClickRegisterButton}>Add Content</Button>
       </div>
       <Table
-        columns={mbtiColumns}
+        columns={columns}
         dataSource={contentsData.contentList}
         rowKey="id"
         pagination={{
