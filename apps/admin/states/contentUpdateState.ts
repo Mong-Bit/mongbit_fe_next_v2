0bbc7cb6abf67afed45e1052bf24319130c7958b
Crystal P <sj8871@gmail.com>
2024-04-26 22:26:52 +0900

    refactor : ContentDetail - Question - 카드의 대답 구분 표시 [대답+1, 대답-1]

--- apps/admin/states/contentUpdateState.ts
+++ apps/admin/states/contentUpdateState.ts
@@ -7,9 +7,9 @@ const mbtiQuestionsArray: MbtiQuestions[] = [];
 for (let i = 0; i < 12; i++) {
   const mbtiQuestion: MbtiQuestions = {
     index: i,
-    question: 'qqq',
-    answerPlus: 'aaa111',
-    answerMinus: 'aaa222',
+    question: '',
+    answerPlus: '',
+    answerMinus: '',
   };
   mbtiQuestionsArray.push(mbtiQuestion);
 }
@@ -36,8 +36,8 @@ const generateMbtiResultState = () => {
 
   return resultTitles.map((title) => ({
     result: title,
-    title: 'ttt',
-    content: 'ccc',
+    title: '',
+    content: '',
     imageUrl: '',
   }));
 };
