634f874b6881aadae1b705ac94e70725063b36d8
Kim SongHyeon <thiagoo@naver.com>
2024-04-29 08:24:46 +0900

    refactor: getData 함수 수정 및 컴포넌트 props 수정

--- apps/client/app/mbtiTest/preview/[id]/page.tsx
+++ apps/client/app/mbtiTest/preview/[id]/page.tsx
@@ -11,10 +11,10 @@ async function getData(url: RouteMbtiTest.getMbtiTestDataProp) {
 }
 
 export default async function Page({ params }: RouteMbtiTest.pageProp) {
-  const mbtiTestData = await getData(`/api/v1/tests/test/${params.id}`).then((response) => response?.dataList);
-  const mbtiTestCommentData = await getData(`/api/v1/test/comments/${params.id}`).then(
-    (response) => response?.dataList,
-  );
+  const mbtiTestData = await getData(`/api/v1/tests/test/${params.id}`);
+  const mbtiTestCommentData = await getData(`/api/v1/test/comments/${params.id}`);
 
-  return <PreviewMbtiTest mbtiTestData={mbtiTestData} mbtiTestCommentData={mbtiTestCommentData} />;
+  return (
+    <PreviewMbtiTest mbtiTestData={mbtiTestData?.dataList.test} mbtiTestCommentData={mbtiTestCommentData?.dataList} />
+  );
 }
