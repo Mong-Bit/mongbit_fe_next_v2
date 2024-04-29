0510f4a373426fcc96a85bca9402adfab00fa4ec
Kim SongHyeon <thiagoo@naver.com>
2024-04-30 08:45:04 +0900

    refactor: api 분리

--- apps/client/app/mbti-test/preview/[id]/page.tsx
+++ apps/client/app/mbti-test/preview/[id]/page.tsx
@@ -1,18 +1,10 @@
-import { fetchClient } from '@/services';
+import { getMbtiTestCommentData, getMbtiTestData } from '@/services';
 
 import PreviewMbtiTest from '@/containers/previewMbtiTest';
 
-async function getData(url: string) {
-  const fetchOption = {
-    url: url,
-    method: 'GET',
-  };
-  return fetchClient(fetchOption);
-}
-
 export default async function Page({ params }: Route.MbtiTestPreviewProp) {
-  const mbtiTestData = await getData(`/api/v1/tests/test/${params.id}`);
-  const mbtiTestCommentData = await getData(`/api/v1/test/comments/${params.id}`);
+  const mbtiTestData = await getMbtiTestData(params.id);
+  const mbtiTestCommentData = await getMbtiTestCommentData(params.id);
 
   return <PreviewMbtiTest mbtiTestData={mbtiTestData?.dataList} mbtiTestCommentData={mbtiTestCommentData?.dataList} />;
 }
