aa98aafeecb462618ef323dbf694a41ca2132fe7
Crystal P <sj8871@gmail.com>
2024-05-02 18:33:22 +0900

    fix : 무한 스크롤 자동 실행 - 첫 데이터 호출 이후, 자동으로 호출 함수가 실행 안됨 - useEffect 에 [obsTarget.current] 넣어줌.. 타겟 인식을 못하는 문제...

--- apps/client/hooks/useInfiniteScroll.ts
+++ apps/client/hooks/useInfiniteScroll.ts
@@ -1,4 +1,3 @@
-import { notFound } from 'next/navigation';
 import { useEffect, useRef, useState } from 'react';
 
 type CallbackProps = (...args: any[]) => Promise<void>;
@@ -6,7 +5,6 @@ type CallbackProps = (...args: any[]) => Promise<void>;
 const useInfiniteScroll = (callback: CallbackProps) => {
   const [isLoading, setIsLoading] = useState(false);
   const [page, setPage] = useState(0);
-  const preventRef = useRef(true);
   const obsTarget = useRef<HTMLDivElement>(null);
 
   const handleObserver = async (entries: IntersectionObserverEntry[]) => {
@@ -14,42 +12,27 @@ const useInfiniteScroll = (callback: CallbackProps) => {
 
     const target = entries[0];
     if (target.isIntersecting) {
-      setIsLoading(true);
-      preventRef.current = false;
       setPage((prevPage) => prevPage + 1);
     }
   };
 
   const fetchData = async () => {
-    try {
-      await callback({ page });
-
-      setIsLoading(false);
-      preventRef.current = true;
-    } catch (err: any) {
-      const status = err.response.status;
-
-      switch (status) {
-        case 404:
-          return notFound();
-        default:
-          throw new Error('Failed to fetch data');
-      }
-    }
+    setIsLoading(true);
+    await callback({ page });
+    setIsLoading(false);
   };
 
   useEffect(() => {
-    fetchData();
-
     const observer = new IntersectionObserver(handleObserver, { threshold: 0.5 });
     if (obsTarget.current) observer.observe(obsTarget.current);
+
     return () => {
       observer.disconnect();
     };
-  }, []);
+  }, [obsTarget.current]);
 
   useEffect(() => {
-    if (page > 0) fetchData();
+    fetchData();
   }, [page]);
 
   return {
