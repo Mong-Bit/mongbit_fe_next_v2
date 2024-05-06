0efdf50f26ad2a52523a39dea1f2bde9c25d0d33
Crystal P <sj8871@gmail.com>
2024-05-06 22:01:57 +0900

    fix: 커밋 되돌리기 복구

--- apps/admin/app/admin/layout.tsx
+++ apps/admin/app/admin/layout.tsx
@@ -1,37 +1,35 @@
 import '../../styles/globals.scss';
 import Layout, { Content, Footer } from 'antd/es/layout/layout';
+import { ErrorBoundary } from 'next/dist/client/components/error-boundary';
 
 import { ROLE_USER } from '@/constants/constant';
 import { decodeToken_ssr } from '@/utils/utils_ssr';
 
+import Error from './error';
 import AdminHeader from '@/components/layout/AdminHeader';
 import DarkModeProvider from '@/components/layout/DarkModeProvider';
 import Navigation from '@/components/layout/Navigation/Index';
 import AccessDeniedPage from '@/components/lib/antd/AccessDeniedPage';
-import KakaoLogin from '@/containers/KakaoLogin';
 
 const RootLayout = ({ children }: React.PropsWithChildren) => {
   const token = decodeToken_ssr();
 
-  switch (true) {
-    case !token.state:
-      return <KakaoLogin />;
-    case token.role === ROLE_USER:
-      return <AccessDeniedPage type={403} />;
-    default:
-      return (
-        <DarkModeProvider>
-          <Layout style={{ minHeight: '100vh', minWidth: 1100 }}>
-            <Navigation />
-            <Layout>
-              <AdminHeader />
-              <Content style={{ margin: '20px 16px 0', minHeight: 500, minWidth: 700 }}>{children}</Content>
-              <Footer style={{ textAlign: 'center', margin: 40 }}>© 2023 MongMoongCrew. All rights reserved</Footer>
-            </Layout>
-          </Layout>
-        </DarkModeProvider>
-      );
-  }
+  if (token.role === ROLE_USER) return <AccessDeniedPage type={403} />;
+
+  return (
+    <DarkModeProvider>
+      <Layout style={{ minHeight: '100vh', minWidth: 1100 }}>
+        <Navigation />
+        <Layout>
+          <AdminHeader />
+          <Content style={{ margin: '20px 16px 0', minHeight: 500, minWidth: 700 }}>
+            <ErrorBoundary errorComponent={Error}>{children}</ErrorBoundary>
+          </Content>
+          <Footer style={{ textAlign: 'center', margin: 40 }}>© 2023 MongMoongCrew. All rights reserved</Footer>
+        </Layout>
+      </Layout>
+    </DarkModeProvider>
+  );
 };
 
 export default RootLayout;
