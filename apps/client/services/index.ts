c26f335a7f9964bd8af1a172ba3e5e6bec6af0b5
Crystal P <sj8871@gmail.com>
2024-05-12 02:14:40 +0900

    refactor: services -> fetchData Porps 타입 수정
    Method Type 추가
    - ype Method = 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH';

--- apps/client/services/index.ts
+++ apps/client/services/index.ts
@@ -1,43 +1,27 @@
 import { notFound } from 'next/navigation';
 
-import { LOGIN } from '@/constants/constant';
+type Method = 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH';
 
-interface createHeadersProrps {
-  contnetType?: string;
-  cacheControl?: string;
+interface FetchOptions {
+  headers: Headers;
+  body: string;
+  params: object;
+  method: Method;
 }
 
-interface fetchDataProps {
-  method: string;
-  headers?: Headers;
-  body?: object;
-  params?: object;
+interface FetchOptionProps extends Omit<FetchOptions, 'body' | 'method'> {
+  body?: { [key: string]: any };
 }
 
-// creatHeaders 추가
-export const createHeaders = ({ contnetType, cacheControl }: createHeadersProrps) => {
-  const headers = new Headers();
-
-  if (typeof sessionStorage === 'undefined') return;
-
-  const loginData = sessionStorage.getItem(LOGIN.MONGBIT);
-  const token = loginData ? JSON.parse(loginData).recoil_logIn[LOGIN.TOKEN_NAME] : '';
-
-  headers.append('Content-Type', contnetType || 'application/json');
-  headers.append('Cache-Control', cacheControl || 'public');
-  headers.append('Authorization', token);
-
-  return headers;
-};
-
-// params 추가
-export const fetchData = async <T>(url: string, { body, ...fetchOptions }: fetchDataProps) => {
-  const options = {
-    body: JSON.stringify(body),
-    ...fetchOptions,
+export const fetchData = async <T>(url: string, method: Method, options?: Partial<FetchOptionProps>) => {
+  const { body, ...restOptions } = options ?? {};
+  const fetchOptions = {
+    method,
+    ...restOptions,
+    ...(body ? { body: JSON.stringify(body) } : undefined),
   };
 
-  const response = await fetch(`${process.env.NEXT_PUBLIC_BE_URL_PROD}${url}`, options);
+  const response = await fetch(`${process.env.NEXT_PUBLIC_BE_URL_PROD}${url}`, fetchOptions);
 
   if (!response.ok) {
     switch (response.status) {
