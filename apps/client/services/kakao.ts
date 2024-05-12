d6267096da2ff478ec053357424f97d6b352d7f0
Crystal P <sj8871@gmail.com>
2024-05-12 17:06:04 +0900

    refactor: headers() 함수 getKakaoLoginAPI() 내부로 이동

--- apps/client/services/kakao.ts
+++ apps/client/services/kakao.ts
@@ -1,12 +1,12 @@
 import { fetchData } from '.';
 import { createHeaders } from './util';
 
-const headers = createHeaders();
-
 export default function redirectKakaoLogin() {
   if (!window.Kakao.isInitialized()) window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_APP_KEY);
   window.location.href = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_REST_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_FE_URL_CLIENT}/login/oauth2/kakao/code&response_type=code`;
 }
 
-export const getKakaoLoginAPI = (code: string) =>
-  fetchData(`/login/oauth2/kakao/code?code=${code}`, 'GET', { headers });
+export const getKakaoLoginAPI = (code: string) => {
+  const headers = createHeaders();
+  return fetchData(`/login/oauth2/kakao/code?code=${code}`, 'GET', { headers });
+};
