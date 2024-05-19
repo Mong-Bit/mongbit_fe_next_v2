c8f70a80cd250aec84d49dd359e78e50fc983890
Crystal P <sj8871@gmail.com>
2024-05-19 12:46:24 +0900

    fix: 머지 후 변경 된 부분 반영

--- apps/client/services/kakao.ts
+++ apps/client/services/kakao.ts
@@ -4,7 +4,7 @@ import { fetchData, createHeaders } from '.';
 
 export default function redirectKakaoLogin() {
   if (!window.Kakao.isInitialized()) window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_APP_KEY);
-  window.location.href = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_REST_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_FE_URL_CLIENT}/login/oauth2/kakao/code&response_type=code`;
+  window.location.href = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_REST_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_FE_URL_PROD}/login/oauth2/kakao/code&response_type=code`;
 }
 
 export const getKakaoLoginAPI = (code: string) => {
