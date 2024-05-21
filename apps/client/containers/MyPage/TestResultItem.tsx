8cfd5ca8cbd361b362e1387e51bc581b16ebab65
Crystal P <sj8871@gmail.com>
2024-05-21 13:51:41 +0900

    refactor: MyPage Ui 파일 MyPage 컴포넌트 폴더로 이동 - 공용 사용하는 것이이난, 마이페이지 내부에서만 사용하고 있어 이동했습니다 - NonMyPage의 경우 통합시키고 타입을 받아 처리하도록 했습니다

---
+++ apps/client/containers/MyPage/TestResultItem.tsx
@@ -0,0 +1,60 @@
+import Image from 'next/image';
+import styled from 'styled-components';
+
+import * as B from '@/styles/base.style';
+import * as L from '@/styles/layout.style';
+import theme from '@/styles/theme';
+import { MbtiResult } from '@/types';
+
+const TestResultItemBox = styled(L.Flex)`
+  height: 185px;
+  border-radius: 1rem;
+  border: 1px solid ${theme.colors.lightGray};
+  overflow: hidden;
+
+  & > div:nth-child(2) {
+    width: 65%;
+    height: 100%;
+    padding: 10px;
+  }
+`;
+
+const TestResultItemTitle = styled(B.TextEllipsis)`
+  color: ${theme.colors.black};
+  height: 30px;
+  line-height: 30px;
+  margin-bottom: 10px;
+  font-size: ${(props) => props.theme.font.size.l};
+  border-bottom: 0.9px solid ${theme.colors.lightGray};
+`;
+
+const TestResultItem = ({ resultData }: { resultData: MbtiResult }) => {
+  const contentTextArray = resultData.content.split('<br>');
+
+  return (
+    <TestResultItemBox>
+      <B.ImageWrap width="35%" height="100%">
+        <Image
+          src={resultData.imageUrl}
+          alt={`${resultData.title} 이미지`}
+          fill
+          sizes="100%"
+          priority
+          style={{
+            objectFit: 'cover',
+          }}
+        />
+      </B.ImageWrap>
+      <div>
+        <TestResultItemTitle>{resultData.title}</TestResultItemTitle>
+        {contentTextArray.slice(0, 6).map((text, index) => (
+          <B.TextEllipsis margin="0 0 5px 0" key={index}>
+            • {text}
+          </B.TextEllipsis>
+        ))}
+      </div>
+    </TestResultItemBox>
+  );
+};
+
+export default TestResultItem;
